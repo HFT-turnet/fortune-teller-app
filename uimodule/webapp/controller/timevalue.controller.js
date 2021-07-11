b842860ff0d5c76b4e745f963dd65edbb27279b6
Hendrik <84269538+HFT-turnet@users.noreply.github.com>
2021-07-11 09:00:53 +0200

    Draft TV App

--- uimodule/webapp/controller/timevalue.controller.js
+++ uimodule/webapp/controller/timevalue.controller.js
@@ -14,9 +14,9 @@ sap.ui.define([
 			//var oHeaders = {
 			//			    "Origin": "https://api.fortune-teller.me"
 			//			};
-			dataset.loadData("https://api.fortune-teller.me/v1/calcs/tv_model", '', true, "GET", null, false,'');
-			
-			console.log(dataset);
+			//dataset.loadData("https://api.fortune-teller.me/v1/calcs/tv_model", '', true, "GET", null, false,'');
+			dataset.loadData("http://localhost:3000/v1/calcs/tv_model", '', true, "GET", null, false,'');
+			//console.log(dataset);
 			this.setModel(dataset);
 		},
 		
@@ -24,6 +24,25 @@ sap.ui.define([
 			//console.log("Denk, Denk");
 			var data=this.getModel();
 			//console.log(data);
+			data.oData.r=data.oData.r/12;
+			//console.log(this.byId("datepickerStart").mProperties.dateValue);//.getproperties("dateValue"));
+			//console.log(this.byId("datepickerEnd").mProperties.dateValue.getMonth());
+			data.oData.periods=this.byId("datepickerEnd").mProperties.dateValue.getMonth()-this.byId("datepickerStart").mProperties.dateValue.getMonth()+ (12 * (this.byId("datepickerEnd").mProperties.dateValue.getFullYear() - this.byId("datepickerStart").mProperties.dateValue.getFullYear()));
+			data.oData.from=this.byId("datepickerStart").mProperties.dateValue.getMonth() +"-"+this.byId("datepickerStart").mProperties.dateValue.getFullYear();
+			data.oData.to=this.byId("datepickerEnd").mProperties.dateValue.getMonth() +"-"+this.byId("datepickerEnd").mProperties.dateValue.getFullYear();
+			//console.log(data);
+			var dataset = new JSONModel();
+			var callpath="http://localhost:3000/v1/calcs/tv_get";
+			//var params = "bkrid="+bkrid+"&postset="+JSON.stringify(posting.oData);
+			var oHeaders = {
+							"Content-Type": "application/json"
+						};
+			//dataset.loadData("http://localhost:3000/v1/calcs/tv_model", params, true, "POST", null, false, oHeaders);
+			dataset.loadData(callpath, JSON.stringify(data.oData), true, "POST", null, false, oHeaders);
+			console.log (dataset);
+			//dataset.loadData("http://localhost:3000/v1/calcs/tv_model", '', true, "POST", null, false,'');
+			//console.log(dataset);
+			this.setModel(dataset);
 		}
     });
 });
