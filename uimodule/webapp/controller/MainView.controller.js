07fafe19c9180fca3068f30cbf0a73bf0fd3be77
Hendrik Thörner <hendrik@thoerni.de>
2021-10-17 22:12:51 +0200

    Update Portal

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -1,4 +1,7 @@
-sap.ui.define(["ft/ui5/ftapp/controller/BaseController"], function (Controller) {
+sap.ui.define([
+		"ft/ui5/ftapp/controller/BaseController",
+		"sap/m/MessageToast"
+	], function (Controller, MessageToast) {
     "use strict";
 
     return Controller.extend("ft.ui5.ftapp.controller.MainView", {
@@ -7,9 +10,20 @@ sap.ui.define(["ft/ui5/ftapp/controller/BaseController"], function (Controller) 
 			// Get Target Frame
 			var module = evt.getSource().getUrl();
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			//console.log(module);
 			oRouter.navTo(module);
 		},
+		
+		callFTsite : function (evt) {
+			// Get Target Frame
+			window.open("https://fortune-teller.me","_self")
+		},
+		
+		notimplemented : function (evt) {
+			// Show message Toast
+			var msg = 'Functionality not implemented yet.';
+			MessageToast.show(msg);
+		},
+		
 		testpress : function () {
 			var test=this.getOwnerComponent().getManifestEntry("/sap.ui5/config/apipath");
 			console.log(test);
